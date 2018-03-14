b34fa04240c18d76da51ad7fae7551ca1a4c1b3f
Jeremy Evans <jeremy.n.evans@gmail.com>
2018-03-14 17:57:58 +0000

    fix4

--- app/controller/chatbot.js
+++ app/controller/chatbot.js
@@ -715,7 +715,7 @@ function intentConfidence(sender, message, extraData) {
 const getResponseMessage = function(data) {
 	logger.debug(getResponseMessage, data)
 	const sender = data.requestData.sender
-	var m = data.memories ? data.memories[0] : null
+	var m = (data.memories ? data.memories[0] : data.card) || null
 	var intent = data.requestData.intent
 	if (intent == 'provideDateTime') intent = 'setTask.dateTime'
 	if (intent == 'provideURL') intent = 'setTask.URL'
